3c11879f72038ca8012bafeb2aec22c72930fbcd
acsauk <acsauk@gmail.com>
2016-06-13 19:18:23 +0100

    Added ch6 exercises and examples

---
+++ ch6-more-about-methods/angryBoss.rb
@@ -0,0 +1,3 @@
+puts 'SO WHADDYA WANT!?'
+request = gets.chomp
+puts 'WHADDAYA MEAN "' + request.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ ch6-more-about-methods/tableOfContents.rb
@@ -0,0 +1,5 @@
+space_width = 40
+puts 'Table of contents'.center(space_width)
+puts 'Chapter 1:  ' + 'Getting started'.ljust(space_width) + 'page' + '1'.rjust(3)
+puts 'Chapter 2:  ' + 'Numbers'.ljust(space_width) + 'page' + '2'.rjust(3)
+puts 'Chapter 3:  ' + 'Letters'.ljust(space_width) + 'page' + '13'.rjust(3)
