c21da9a46d5d39a632199c8c6dc54e9563b5e799
acsauk <acsauk@gmail.com>
2016-06-13 15:09:22 +0100

    Adding ch5 exercises and examples

---
+++ ch5-mixing-it-up/betterNumber.rb
@@ -0,0 +1,4 @@
+puts "Hi! What is your favourite number??"
+faveNum = gets.chomp
+betterNum = faveNum.to_i + 1
+puts "I see...are you sure that " + betterNum.to_s + " isn't a better choice? Just sayin..."
---
+++ ch5-mixing-it-up/nameGreeter.rb
@@ -0,0 +1,7 @@
+puts "Hello there! What is your first name?"
+firstName = gets.chomp
+puts "Thanks. And your middle name? If you don't have one just hit enter."
+middleName = gets.chomp
+puts "Great - finally what is your last name?"
+lastName = gets.chomp
+puts "Nice to meet you " + firstName + " " + middleName + " " + lastName + "!"
