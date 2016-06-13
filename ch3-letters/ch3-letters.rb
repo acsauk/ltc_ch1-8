5d997055a8cdc298f7981ce08a861480279c742c
acsauk <acsauk@gmail.com>
2016-06-13 14:15:50 +0100

    Added chapter three working examples

---
+++ ch3-letters/ch3-letters.rb
@@ -0,0 +1,24 @@
+puts 'Hello, world!'
+puts ''
+puts "Good-bye."
+
+puts ""
+
+puts "I like" + " apple pie."
+puts "blink " * 4
+
+puts ""
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts ""
+
+puts "You\'re swell"
+puts "backlask at the end of a string: \\"
+puts "up\\down"
+puts "up\down"
