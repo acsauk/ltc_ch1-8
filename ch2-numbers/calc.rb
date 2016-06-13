1dc1d8414633555d074acf352124bc5914b703bc
acsauk <acsauk@gmail.com>
2016-06-13 12:18:07 +0100

    First commit - added ch1 and 2 folders and ch2 completed exercise

---
+++ ch2-numbers/calc.rb
@@ -0,0 +1,39 @@
+puts 1 + 2
+
+puts ""
+
+puts 1.0 + 2.0
+puts 2.0 + 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts ""
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts ""
+
+#Hours in a year
+
+puts "There are #{24*365} hours in a year"
+
+puts ""
+
+#Minutes in a decade
+
+puts "There are #{(8760 * 60) * 10} minutes in a decade"
+
+puts ""
+
+#Age in seconds
+
+puts "I am #{((8760 * 60) * 60) * 30} seconds old"
+
+puts ""
+
+#Authors ages converted from seconds to years
+
+puts "If the author is 1160000000 seconds old, he is #{1160000000 / ((8760 * 60) * 60)} years old"
